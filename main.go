cdbe911a8ce3416332b16a82d4e03b0d7d363a25
Braian Corrêa <00.braian.dev@gmail.com>
2021-05-16 18:07:42 -0300

    [Feat] Turn On/Off Lightbulb

--- main.go
+++ main.go
@@ -26,6 +26,28 @@ func main() {
 		json.NewEncoder(responseWriter).Encode(lightbulbs)
 	})
 
+	http.HandleFunc("/lightbulbs/switch", func(responseWriter http.ResponseWriter, request *http.Request) {
+		responseWriter.Header().Set("Content-Type", "application-json")
+
+		name := request.URL.Query().Get("name")
+		if name == "" {
+			responseWriter.WriteHeader(http.StatusBadRequest)
+			responseWriter.Write([]byte(`{"message":"a light bulb name should be provided as the value of a 'name' querystring parameter"}`))
+			return
+		}
+
+		if _, keyExists := lightbulbs[name]; !keyExists {
+			responseWriter.WriteHeader(http.StatusNotFound)
+			responseWriter.Write([]byte(`{"message":"a light bulb with the provided name doesn't exist"}`))
+			return
+		}
+
+		lightbulbs[name] = !lightbulbs[name]
+
+		responseWriter.WriteHeader(http.StatusOK)
+		json.NewEncoder(responseWriter).Encode(lightbulbs)
+	})
+
 	fmt.Println("http server listening on localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
